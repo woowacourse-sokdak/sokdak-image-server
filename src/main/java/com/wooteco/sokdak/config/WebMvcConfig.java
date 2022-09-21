13662ba9b3391224114678237140bc68f1ce3e75
EastHShin <xldk78@gmail.com>
2022-09-22 01:49:59 +0900

    feat: 외부 경로의 리소스에 대한 접근 허용

---
+++ src/main/java/com/wooteco/sokdak/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.wooteco.sokdak.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file://home/ubuntu/images/");
+    }
+}
