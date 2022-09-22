1ee8bee019ca9fec99f179225751f20ca976e806
EastHShin <xldk78@gmail.com>
2022-09-22 11:26:34 +0900

    fix: cors 허용

--- src/main/java/com/wooteco/sokdak/config/WebMvcConfig.java
+++ src/main/java/com/wooteco/sokdak/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedHeaders("http://localhost:3000",
+                .allowedOriginPatterns("http://localhost:3000",
                         "http://sokdaksokdak.com",
                         "http://www.sokdaksokdak.com",
                         "https://sokdaksokdak.com",
